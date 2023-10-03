942638ee61d28c275fe48c8af7b59949089e1428
ChangWhan Lee <zeta0707@gmail.com>
2023-10-03 14:21:35 +0900

    Tune navigation2 parameter
    1. Tune navigation2 parameter
    2. sleMotor.sh change esp32 ino file as well, add MOTOR_TYPE swap feature

--- script/selMotor.sh
+++ script/selMotor.sh
@@ -10,10 +10,14 @@ fi
 
 if [ "$1" == "178" ]; then
     echo "178RPM motor set"
+    sed -i 's/MOTOR_TYPE MOTOR_60RPM/MOTOR_TYPE MOTOR_178RPM/g' ../arduino/motorController32Test.ino
+    sed -i 's/MOTOR_TYPE MOTOR_60RPM/MOTOR_TYPE MOTOR_178RPM/g' ../arduino/motorEncLedMpuRos32.ino
     sed -i 's/1860.0/620.0/g' ../monicar2_localization/param/initPose0.yaml
     sed -i 's/1860.0/620.0/g' ../monicar2_localization/param/initPose1.yaml
 else
     echo "60RPM motor set"
+    sed -i 's/MOTOR_TYPE MOTOR_178RPM/MOTOR_TYPE MOTOR_60RPM/g' ../arduino/motorController32Test.ino
+    sed -i 's/MOTOR_TYPE MOTOR_178RPM/MOTOR_TYPE MOTOR_60RPM/g' ../arduino/motorEncLedMpuRos32.ino
     sed -i 's/620.0/1860.0/g' ../monicar2_localization/param/initPose0.yaml
     sed -i 's/620.0/1860.0/g' ../monicar2_localization/param/initPose1.yaml
 fi 
